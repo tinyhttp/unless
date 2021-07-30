568ee1ab27909e5644c0ce53e52801e5d03e6d71
v1rtl <pilll.pl22@gmail.com>
2021-07-30 13:15:45 +0300

    feat: move unless to a separate repo

--- README.md
+++ README.md
@@ -6,17 +6,46 @@
 
 </div>
 
-Description.
+Unless middleware for tinyhttp that executes a middleware conditionally.
 
 ## Install
 
 ```sh
-pnpm i pkg
+pnpm i @tinyhttp/unless
 ```
 
-[npm-url]: https://npmjs.com/package/@tinyhttp/favicon
-[github-actions]: https://github.com/tinyhttp/favicon/actions
-[gh-actions-img]: https://img.shields.io/github/workflow/status/tinyhttp/favicon/CI?style=for-the-badge&logo=github&label=&color=hotpink
-[cov-img]: https://img.shields.io/coveralls/github/tinyhttp/favicon?style=for-the-badge&color=hotpink
-[cov-url]: https://coveralls.io/github/tinyhttp/favicon
-[npm-img]: https://img.shields.io/npm/dt/@tinyhttp/favicon?style=for-the-badge&color=hotpink
+## API
+
+### `unless(middleware, (UnlessMiddlewareOptions | CustomUnless))`
+
+The `UnlessMiddlewareOptions` object can include:
+
+- `method` - string or array of strings that describe forbidden http methods such as GET, POST, PUT etc...
+- `path` - array of strings, Regex and objects that include `url` and `methods` properties, which will be compared against the request.
+- `ext` - string or array of strings that describe forbidden path ends (e.g. in `/user/123` it will check against `/123`).
+
+The `CustomUnless` is a function that receives a Request object and returns a boolean. The result of the function will determine if the execution of the middleware is skipped.
+
+## Example
+
+```ts
+import { App } from '@tinyhttp/app'
+import { unless } from '@tinyhttp/unless'
+import { cors } from '@tinyhttp/cors'
+
+const app = new App()
+
+app
+  .use(unless(cors(),  { method: ['GET', 'POST'] }))
+  .use(unless(cors(), { ext: '/public' }))
+  .use(unless(cors(), (req) => req.method === 'GET')
+  .use(unless(cors(), { path: ['/content/public', /user/, { url: '/public', methods: ['GET'] }] })
+  .listen(3000)
+```
+
+[npm-url]: https://npmjs.com/package/@tinyhttp/unless
+[github-actions]: https://github.com/tinyhttp/unless/actions
+[gh-actions-img]: https://img.shields.io/github/workflow/status/tinyhttp/unless/CI?style=for-the-badge&logo=github&label=&color=hotpink
+[cov-img]: https://img.shields.io/coveralls/github/tinyhttp/unless?style=for-the-badge&color=hotpink
+[cov-url]: https://coveralls.io/github/tinyhttp/unless
+[npm-img]: https://img.shields.io/npm/dt/@tinyhttp/unless?style=for-the-badge&color=hotpink
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,6 +1,7 @@
 import { suite } from 'uvu'
 import * as assert from 'uvu/assert'
-import { add } from '../src/index'
+import type { Request, Response } from '@tinyhttp/app'
+import { unless, UnlessMiddlewareOptions, CustomUnless } from '../src/index'
 
 function describe(name: string, fn: (...args: any[]) => void) {
   const s = suite(name)
@@ -8,8 +9,103 @@ function describe(name: string, fn: (...args: any[]) => void) {
   s.run()
 }
 
-describe('add', (it) => {
-  it('adds', () => {
-    assert.equal(add(4, 5), 9)
+import { createServer } from 'http'
+import { makeFetch } from 'supertest-fetch'
+
+function exampleMiddleware(_: Request, res: Response): void {
+  res.setHeader('MW', 1)
+}
+
+function InitAppAndTestUnless(options: UnlessMiddlewareOptions | CustomUnless) {
+  const app = createServer((req, res) => {
+    unless({ handler: exampleMiddleware, type: 'mw' }, options)(req as Request, res as Response, () => {
+      return
+    })
+    res.statusCode = 200
+    res.end()
+  })
+
+  const fetch = makeFetch(app)
+
+  return fetch
+}
+
+describe('Unless Middleware path parameter test', (it) => {
+  it("should execute if path string doesn't match", async () => {
+    const fetch = InitAppAndTestUnless({ path: '/test' })
+
+    await fetch('/').expectStatus(200).expectHeader('MW', 1)
+  })
+  it("should execute if path string doesn't match", async () => {
+    const fetch = InitAppAndTestUnless({ path: /test/ })
+
+    await fetch('/').expectStatus(200).expectHeader('MW', 1)
+  })
+  it('should execute if only object url has a match', async () => {
+    const fetch = InitAppAndTestUnless({ path: ['/test', { url: '/', methods: ['POST'] }] })
+
+    await fetch('/').expectStatus(200).expectHeader('MW', 1)
+  })
+  it('should execute if only object methods has a match', async () => {
+    const fetch = InitAppAndTestUnless({ path: ['/test', { url: '/tes', methods: ['GET'] }] })
+
+    await fetch('/').expectStatus(200).expectHeader('MW', 1)
+  })
+
+  it('should not execute if path string match', async () => {
+    const fetch = InitAppAndTestUnless({ path: [/\/test/, '/'] })
+
+    await fetch('/').expectStatus(200).expectHeader('MW', undefined)
+  })
+  it('should not execute if path regex match', async () => {
+    const fetch = InitAppAndTestUnless({ path: ['/test', /\//] })
+
+    await fetch('/').expectStatus(200).expectHeader('MW', undefined)
+  })
+  it('should not execute if object url and method match', async () => {
+    const fetch = InitAppAndTestUnless({ path: ['/test', { url: '/', methods: ['POST', 'GET'] }] })
+
+    await fetch('/').expectStatus(200).expectHeader('MW', undefined)
+  })
+})
+
+describe('Unless Middleware ext parameter test', (it) => {
+  it("should execute if ext doesn't match", async () => {
+    const fetch = InitAppAndTestUnless({ ext: ['/users/123', '/users'] })
+
+    await fetch('/users/123').expectStatus(200).expectHeader('MW', 1)
+  })
+
+  it('should not execute if ext match', async () => {
+    const fetch = InitAppAndTestUnless({ ext: ['/users/123', '/123'] })
+
+    await fetch('/users/123').expectStatus(200).expectHeader('MW', undefined)
+  })
+})
+
+describe('Unless Middleware method parameter test', (it) => {
+  it("should execute if method string doesn't match", async () => {
+    const fetch = InitAppAndTestUnless({ method: ['POST', 'PUT'] })
+    await fetch('/').expectStatus(200).expectHeader('MW', 1)
+  })
+
+  it('should not execute if method string match', async () => {
+    const fetch = InitAppAndTestUnless({ method: ['POST', 'GET'] })
+
+    await fetch('/').expectStatus(200).expectHeader('MW', undefined)
+  })
+})
+
+describe('Unless Middleware custom parameter test', (it) => {
+  it('should execute if function returns false', async () => {
+    const fetch = InitAppAndTestUnless(() => false)
+
+    await fetch('/').expectStatus(200).expectHeader('MW', 1)
+  })
+
+  it('should not execute if function returns true', async () => {
+    const fetch = InitAppAndTestUnless(() => true)
+
+    await fetch('/').expectStatus(200).expectHeader('MW', undefined)
   })
 })
